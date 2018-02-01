a252f5028ccf1c23af17c57819b1e714952d6dd7
smilingkite <mail@katinkahesselink.net>
2018-02-01 11:31:04 +0100

    experiments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Card(letter, number, deckNo){
+function Card(letter, number, deckNo) {
   this.letter = letter
   this.number = number
   // deckNo = 0 : card is in the deck, can be dealt
@@ -13,8 +15,8 @@ function Card(letter, number, deckNo){
 }
 const letters = ["A", "B", "C", "D", "E", "F", "G"]
 
-function deckCards(deck = []){
-  for (let i = 0; i< 7; i++){
+function deckCards(deck = []) {
+  for (let i = 0; i < 7; i++) {
     for (let j = 1; j < 5; j++) {
       const card = new Card(letters[i], j, 0)
       deck.push(card)
@@ -27,68 +29,78 @@ var deck = deckCards(deck)
 // console.log(deck)
 
 // leads to problems when called for second player
-const dealCard = (playerNo, deck) => {
+function dealCard (playerNo, deck) {
   // deal a random card: assign one card to the player
   const deckNo = playerNo
   const random = Math.floor(Math.random() * deck.length)
-  if (deck[random].deckNo === 0){
+  if (deck[random].deckNo === 0) {
     return deck.map(card => {
       if (card.deckNo === 0 && deck[random] === card) {
         return {
-          ...card, 
+          ...card,
           deckNo
         }
       } else {
-        return {...card}
+        return { ...card
+        }
       }
     })
-  }
-  else dealCard(playerNo, deck)
+  } else dealCard(playerNo, deck)
 }
-
+var changeOneCard = dealCard(1,deck)
+console.log(changeOneCard)
+var changeSecondCard = dealCard(2, changeOneCard)
+console.log(changeSecondCard)
 // doesn't take conditions into account enough
-const moveCard = (deck, playerNo, cardNo, from) => {
-  if (from === 0 ){
-    deck[cardNo].deckNo = playerNo
-  }
-  return deck
-}
+// const moveCard = (deck, playerNo, cardNo, from) => {
+//   if (from === 0 ){
+//     deck[cardNo].deckNo = playerNo
+//   }
+//   return deck
+// }
 
 // should use moveCard or dealCard
-function dealCards(player, deck, no){
+function dealCards(player, deck, no) {
   no--
   var newDeck
-  const from = 0
-  const random = Math.floor(Math.random() * deck.length)
-  if (deck[random].deckNo !== from) dealCards(player, deck, no)
+  // const from = 0
   if (no >= 0) {
-    newDeck = moveCard(deck, player, random, from)
+    newDeck = dealCard(player, deck)
     return dealCards(player, newDeck, no)
   } else {
     return deck
   }
 }
 
-const player1 = 1;
-const player2 = 2;
+// const player1 = 1;
+// const player2 = 2;
 // const newDeck3 = dealCards(player1, deck, 6);
 
+// console.log(newDeck3)
+
 // TypeError: Cannot read property 'length' of undefined (line 34, 57, 55)
-const newDeck4 = dealCards(player2, dealCards(player1, deck, 6), 6);
-console.log(newDeck4);
+// const newDeck4 = dealCards(player2, newDeck3, 6);
+// console.log(newDeck4);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      img src = {
+        logo
+      }
+      className = "App-logo"
+      alt = "logo" / >
+      <
+      h1 className = "App-title" > Welcome to React < /h1> <
+      /header> <
+      p className = "App-intro" >
+      To get started, edit < code > src / App.js < /code> and save to reload. <
+      /p> <
+      /div>
     );
   }
 }
