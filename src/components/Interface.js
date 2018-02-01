1aab6f7dd31174575fc5be0fcb71eff45b8cb4d1
smilingkite <mail@katinkahesselink.net>
2018-02-01 15:11:39 +0100

    Add components for views

---
+++ src/components/Interface.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+const Interface = ({onNewCard=f=>f}) => {
+  let _kaart;
+  const submit = e => {
+    e.preventDefault();
+    onNewCard(_kaart.value);
+    _kaart.value = '';
+    _kaart.focus();
+  };
+  return (
+    <div className="interface">
+      {/* <span className="boodschap">{message}</span>
+          */}
+      <form onSubmit={submit}>
+        <input ref={input => _kaart = input}
+          type="text"
+          placeholder="letter nummer" required />
+        <button>vraag kaart</button>
+      </form>
+    </div>
+  );
+};
+
+export default Interface;
---
+++ src/components/Player.js
@@ -0,0 +1,42 @@
+var React = require('react');
+
+class PlayerComponent extends React.Component{
+  
+  render(){
+    const {name, hand, kwartet, turn} = this.props; 
+    return (
+      <div className={`speler ${turn}`}>
+        <h2>{name}</h2>
+        <div className="kaarten">
+          <p>Kaarten</p>
+          <ul className="hand">
+            {
+              hand.map((kaart, i) => 
+                <li key={i}  className="kaart">{kaart.letter}{kaart.number}</li>
+              )
+            }
+          </ul>
+        </div>
+        <div className="kwartettenlijst">
+
+          { kwartet.length > 0 && 
+            <div>
+              <p>Kwartetten</p>
+              <ul className="kwartetten">
+                {
+                  kwartet.map((kaart, i) =>
+                    <li key={i} className="kwartet">{kaart}</li>
+                  )
+                }
+              </ul>
+            </div>
+          }
+        </div>
+        
+
+      </div>
+    );
+  }
+}
+
+export default PlayerComponent;
