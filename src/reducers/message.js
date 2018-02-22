51af856fc32d48c41c0a99f1b135149dfc7d94c2
smilingkite <mail@katinkahesselink.net>
2018-02-22 14:08:59 +0100

    wip select message through action creator

--- src/reducers/message.js
+++ src/reducers/message.js
@@ -4,8 +4,10 @@ import {SELECT_MESSAGE} from '../actions/selectMessage'
 export default (state = messages[0].value, { type, payload } = {}) => {
   
   switch(type) {
-    
+    case SELECT_MESSAGE :
+      return messages.filter((message, key) => message.key === payload.key? message.value : messages[0].value)
+
     default:
-    return state
+      return state
   }
 }
