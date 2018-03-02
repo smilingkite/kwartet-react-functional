96f34927702602f430a45d9689f67452af02a456
smilingkite <mail@katinkahesselink.net>
2018-03-02 10:59:15 +0100

    WIP - can't find store in changeTurn reducer

--- src/actions/changeTurn.js
+++ src/actions/changeTurn.js
@@ -1,5 +1,21 @@
-export const CHANGE_TURN = 'CHANGE_TURN'
+import store from '../store';
 
+// const selectHand = (deck, handNo) => deck.filter(card => card.deckNo === handNo)
+let deck = store
+console.log('STORE', store)
+
+// const findRandomUnassignedCard = (deck) => {
+//   if (selectHand(deck,0).length === 0) return {}
+//   const random = Math.floor(Math.random() * deck.length)
+
+//   if (deck[random].deckNo === 0) {
+//     return deck[random]
+//   } else return findRandomUnassignedCard(deck)
+// }
+
+// const card = findRandomUnassignedCard(deck)
+
+export const CHANGE_TURN = 'CHANGE_TURN'
 export default (card) => {
   return {
     type: CHANGE_TURN,
