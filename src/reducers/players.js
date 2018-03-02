2128cf6814ee426d673a86864f5b895378ee98fe
smilingkite <mail@katinkahesselink.net>
2018-03-02 10:22:02 +0100

    implement change turn

--- src/reducers/players.js
+++ src/reducers/players.js
@@ -1,11 +1,28 @@
 import players from '../data/players'
 import { CHANGE_TURN } from '../actions/changeTurn'
 
-export default (state = players, {type, payload} = {}) => {
+export default (state = players, {type} = {}) => {
+  let playerTurn = players.playerTurnID
+  // let otherPlayer = players.otherPlayerID
+  let player1 = players.player1
+  let player2 = players.player2
+  let newPlayerTurn, newOtherPlayer
 
   switch(type) {
     case CHANGE_TURN:
-      // change turn
+      if (playerTurn === player1.idNo) {
+        newPlayerTurn = player2.idNo;
+        newOtherPlayer = player1.idNo;
+      }
+      else {
+        newPlayerTurn = player1.idNo;
+        newOtherPlayer = player2.idNo;
+      }
+      return {
+        ...state,
+        otherPlayerID: newOtherPlayer,
+        playerTurnID: newPlayerTurn
+      }
 
     default: 
       return state  
