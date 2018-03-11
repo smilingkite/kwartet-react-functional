5b2634572abe41caa8204a6a69c32edbe8cd3983
smilingkite <mail@katinkahesselink.net>
2018-03-11 18:20:24 +0100

    code clean up

--- src/reducers/deck.js
+++ src/reducers/deck.js
@@ -24,16 +24,13 @@ export default (state = deck, {type, payload} = {}) => {
 
       const updatedItems = state.map(item => {
         if(item.letter === payload.letter && item.number === payload.number){
-          if (selectHand(state, payload.deckNo).length > 0){
-            // Check whether player is allowed to ask for this card 
 
-            if (item.deckNo === 0) {
-              // if card in otherplayerhand > use payload card in deck 
-              // (already has deckNo of current player)
-              return item
-            }
-            else return payload
+          if (item.deckNo === 0) {
+             // if card in otherplayerhand > use payload card in deck 
+             // (already has deckNo of current player)
+             return item
           }
+          else return payload
         }
         return item
       })
