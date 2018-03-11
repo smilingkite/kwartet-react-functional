7d677aa800737ad02082613014536c79c5db1754
smilingkite <mail@katinkahesselink.net>
2018-03-11 16:28:09 +0100

    code clean up

--- src/actions/changeTurn.js
+++ src/actions/changeTurn.js
@@ -1,22 +1,3 @@
-// import store from '../store';
-
-// const selectHand = (deck, handNo) => deck.filter(card => card.deckNo === handNo)
-// let stored = getState
-// console.log('STORE', store) // WHY undefined???
-
-// const findRandomUnassignedCard = (deck) => {
-//   if (selectHand(deck,0).length === 0) return {}
-//   const random = Math.floor(Math.random() * deck.length)
-
-//   if (deck[random].deckNo === 0) {
-//     return deck[random]
-//   } else return findRandomUnassignedCard(deck)
-// }
-
-// const card = findRandomUnassignedCard(deck)
-
-// NB: asign to hand playerTurn???? (would help keep reducer logic simple)
-
 export const CHANGE_TURN = 'CHANGE_TURN'
 export default (card) => {
   return {
