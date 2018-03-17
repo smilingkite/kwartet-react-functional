572a0e0b547d49881b5fae20c8fa82e9506bc738
smilingkite <mail@katinkahesselink.net>
2018-03-17 19:34:03 +0100

    styling it up

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -9,35 +9,31 @@ const Interface = ({onNewCard=f=>f}) => {
     e.preventDefault();
     let kaart = _kaart.letters.value + _kaart.cijfers.value
     onNewCard(kaart);
-    // _kaart.value = '';
-
-    // _kaart.focus();
-    console.log(kaart)
   };
   const handleChange = e => {
-    // let name = e.target.name
     _kaart[e.target.name]= e.target.value;
-    // console.log(_kaart.letters.value , _kaart.cijfers.value)
   };
 
   
   return (
     <div className="interface">
       <form onSubmit={submit}  ref={input => _kaart = input}>
+        <div className="letters form">
         {letters.map((letter, i) =>
-        <label key={i}>
-          <input type="radio" name ="letters" value={letter} onChange={handleChange}/>
-            {letter}
-        </label>
+          <label key={i}>
+            <input type="radio" name ="letters" value={letter} onChange={handleChange}/>
+              {letter}
+          </label>
         )}
+        </div>
+        <div className="cijfers form">
         { [1,2,3,4].map((cijfer, j) => 
           <label key={j*10}>
             <input type="radio" name="cijfers" value={cijfer} onChange={handleChange}/>
             {cijfer}
-          </label>)
-          
+          </label>) 
         }
-
+        </div>
         <button>vraag kaart</button>
       </form>
     </div>
