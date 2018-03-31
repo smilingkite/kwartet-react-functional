fd05799d63c858fcdc86d12891e45aa755777821
smilingkite <mail@katinkahesselink.net>
2018-03-31 14:44:19 +0200

    adapt messages to situation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {
   PureComponent
 } from 'react';
 import { connect } from 'react-redux';
-
+import messages from './data/messages';
 import { SELECT_MESSAGE } from './actions/selectMessage';
 import { MOVE_CARD } from './actions/moveCard';
 import { CHANGE_TURN } from './actions/changeTurn';
@@ -27,11 +27,24 @@ class App extends PureComponent {
     this.game = this.game.bind(this);
   }
 
-  changeMessage = (message) => {
-    this.props.dispatch({type: SELECT_MESSAGE, payload: message})
+  // changeMessage = (message) => {
+  //   this.props.dispatch({type: SELECT_MESSAGE, payload: message})
+  // }
+
+  changeMessage = (message, extra) => {
+    let MessageObjectArray = messages.filter((m) => m.key === message)
+    let newMessage = MessageObjectArray[0].value.replace("XXX", extra);
+    console.log(extra)
+    console.log(newMessage)
+    this.props.dispatch({type: SELECT_MESSAGE, payload: newMessage})
   }
   
   selectHand = (deck, handNo) => deck.filter(card => card.deckNo === handNo)
+  
+  selectPlayerName = (playerIdNo) => {
+    if (playerIdNo === 1) return this.props.players.player1.name
+    else return this.props.players.player2.name
+  }
 
   checkKwartet = (hand, playerID) => {
     let selectLetter = (hand, letter) => hand.filter(card => card.letter === letter);
@@ -80,17 +93,17 @@ class App extends PureComponent {
     // also check of allowed to ask for card, in order to change turn if not.
     if (validCard) {
       if (!this.isLegitAskedCard(deck, playerTurnID, kaartuitvoer)) {
-        this.changeMessage('beurtWissel');
+        this.changeMessage('beurtWissel', this.selectPlayerName(otherPlayerID));
         this.onChangeTurn(deck, playerTurnID, otherPlayerID);
       } else {
         // If card not in otherplayer hand, changeTurn
         let otherPlayerHand = this.selectHand(deck, otherPlayerID)
         if (this.hasCardInHand(otherPlayerHand, kaartuitvoer)) {
           this.props.dispatch({type: MOVE_CARD, payload: kaartuitvoer})
-          this.changeMessage('goeieGok')
+          this.changeMessage('goeieGok' , this.selectPlayerName(otherPlayerID))
           this.checkKwartet(this.selectHand(deck, playerTurnID), playerTurnID);
         } else {
-          this.changeMessage('beurtWissel');
+          this.changeMessage('beurtWissel', this.selectPlayerName(otherPlayerID));
           this.onChangeTurn(deck, playerTurnID, otherPlayerID);
         }
         validCard = false;
@@ -99,14 +112,14 @@ class App extends PureComponent {
   }
 
   render() {
-    const {game, selectHand} = this; // functions
+    const {game, selectHand, selectPlayerName} = this; // functions
     const {message, deck, kwartetList} = this.props;
-    const {playerTurnID, player1, player2} = this.props.players;
+    const {playerTurnID} = this.props.players;
 
-    const selectPlayerName = (playerIdNo) => {
-      if (playerIdNo === 1) return player1.name
-      else return player2.name
-    }
+    // const selectPlayerName = (playerIdNo) => {
+    //   if (playerIdNo === 1) return player1.name
+    //   else return player2.name
+    // }
 
     return ( 
       <div className = "App" >
--- src/reducers/message.js
+++ src/reducers/message.js
@@ -5,8 +5,8 @@ export default (state = messages[0].value, { type, payload } = {}) => {
   
   switch(type) {
     case SELECT_MESSAGE :
-      let MessageObjectArray = messages.filter((message) => message.key === payload)
-      return MessageObjectArray[0].value
+      // let MessageObjectArray = messages.filter((message) => message.key === payload)
+      return payload
 
     default:
       return state
